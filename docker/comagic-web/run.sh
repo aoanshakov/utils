265db3ab3b042b5f4b6578be51578ec9c81f184d
Alexander Anshakov <a.anshakov@uiscom.ru>
2019-12-03 14:25:25 +0000

    Исправь ошибки сборки контейнеров для ЛК и тестов

--- docker/comagic-web/run.sh
+++ docker/comagic-web/run.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+action="runclient"
+export COMAGIC_ENV_USER=""
+
+while [ -n "$1" ]
+do
+    if [ "$1" = "-a" ]
+    then
+        action=$2
+        shift
+    elif [ "$1" = "-u" ]
+    then
+        export COMAGIC_ENV_USER=$2
+        shift
+    fi
+
+    shift
+done
+
 if [ ! -d /usr/local/src ]
 then
     echo "Workspace directory is not exist"
@@ -10,23 +28,22 @@ if [ ! -d "/usr/local/src/comagic_web" ]
 then
     initialize
     build
-    configure
+    config
 fi
 
-if [ -z "$1" ]
+if [ "$action" = "initialize" ]
+then
+    exit 0
+elif [ "$action" = "runclient" ]
+then
+    runclient
+elif [ "$action" = "build" ]
+then
+    build
+elif [ "$action" = "config" ]
+then
+    config
+elif [ "$action" = "bash" ]
 then
     bash
-else
-    if [ "$1" = "runclient" ]
-    then
-        runclient
-    elif [ "$1" = "build" ]
-    then
-        build
-    elif [ "$1" = "configure" ]
-    then
-        configure
-    else
-        bash
-    fi
 fi
