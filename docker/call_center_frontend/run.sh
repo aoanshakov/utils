eaefedf1234d4b2b29a600415fc95323bcc2d8b2
Alexander Anshakov <a.anshakov@uiscom.ru>
2019-12-03 10:50:05 +0300

    Докер для сборки фронтенда антихаоса

---
+++ docker/call_center_frontend/run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [ ! -d /usr/local/src ]
+then
+    echo "/usr/local/src directory is not exist"
+    exit 1
+fi
+
+user=''
+action=''
+server=''
+
+while [ -n "$1" ]
+do
+    if [ "$1" = "-u" ]
+    then
+        user=$2
+        shift
+    elif [ "$1" = "-s" ]
+    then
+        server=$2
+    else
+        action=$1
+    fi
+
+    shift
+done
+
+export CALL_CENTER_SERVER_DOMAIN=$server
+export CALL_CENTER_APP_DOMAIN=127.0.0.1
+
+if [ ! -d "/usr/local/src/call_center_frontend" ]
+then
+    cd /usr/local/src
+    git clone git@git.dev.uis.st:web/call_center_frontend.git
+    cd call_center_frontend
+
+    npm install
+    build
+
+    if [ -n "$user" ]
+    then
+        chown -R $user:$user /usr/local/src/call_center_frontend
+    fi
+
+    if [ "$action" = "build" ]
+    then
+        action=''
+    fi
+fi
+
+cd /usr/local/src/call_center_frontend
+
+if [ -z "$action" ]
+then
+    bash
+else
+    if [ "$action" = "runserver" ]
+    then
+        runserver
+        bash
+    elif [ "$action" = "build" ]
+    then
+        build
+    else
+        bash
+    fi
+fi
