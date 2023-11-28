a468f640dea369264ae0a8b59e5ebd68650b0a74
Alexander Anshakov <a.anshakov@uiscom.ru>
2023-11-28 11:32:43 +0000

    Добавь возможность линковать модули

--- omni-tests/utils/index.js
+++ omni-tests/utils/index.js
@@ -3,6 +3,9 @@ const { Args, isOneOf, isListOf, isString } = require('./arguments'),
     execute = require('./execute'),
     mkdir = require('./mkdir'),
     write = require('./write');
+    JSON3 = require('json3'),
+    readJson = path => JSON.parse(fs.readFileSync(path)),
+    encodeJson = content => JSON3.stringify(content, null, 4);
 
 const {
     src,
@@ -11,67 +14,146 @@ const {
 
 const actions = {},
     rmVerbose = target => `if [ -e ${target} ]; then rm -rvf ${target}; fi`,
-    modulePath = name => `${src}/${name}`;
+    modulePath = directory => `${src}/${directory}`;
 
 actions['fix-permissions'] =
     [`if [ -n "$APPLICATION_OWNER" ]; then chown -R $APPLICATION_OWNER:$APPLICATION_OWNER $1 ${src}; fi`];
 
 const modules = [{
-    module: 'comagic-app/desktop',
-    name: 'desktop',
+    repository: 'comagic-app/desktop',
+    directory: 'desktop',
     envFileName: '.env',
     script: 'dev',
     branch: '{stand}'
 }, {
-    module: 'chats/frontend',
-    name: 'chats',
+    repository: 'chats/frontend',
+    directory: 'chats',
     param: 'REACT_APP_MODULE_CHATS',
 }, {
-    module: 'softphone/frontend',
-    name: 'softphone',
+    repository: 'softphone/frontend',
+    directory: 'softphone',
     param: 'REACT_APP_MODULE_SOFTPHONE',
+    linkedModules: [{
+        module: '@comagic/softphone-widget',
+        repository: 'web/sip_lib',
+        directory: 'softphone-widget',
+        linkedModules: [{
+            module: '@comagic/softphone-core',
+            repository: 'web/uis_webrtc',
+            directory: 'softphone-core',
+        }],
+    }],
 }, {
-    module: 'web/comagic_app_modules/contacts',
-    name: 'contacts',
+    repository: 'web/comagic_app_modules/contacts',
+    directory: 'contacts',
     param: 'REACT_APP_MODULE_CONTACTS',
 }, {
-    module: 'web/comagic_app_modules/operator-workplace',
-    name: 'operator-workplace',
+    repository: 'web/comagic_app_modules/operator-workplace',
+    directory: 'operator-workplace',
     param: 'REACT_APP_MODULE_EMPLOYEES',
 }];
 
-const submoduleNames = modules.map(({ module, name }) => name).filter(name => name != 'desktop'),
-    replaceWithStand = ({ value, params }) => value.split('{stand}').join(params.stand || '');
+const moduleNames = modules.map(({ repository, directory }) => directory),
+    submoduleNames = moduleNames.filter(directory => directory != 'desktop'),
+    replaceWithStand = ({ value, params }) => value.split('{stand}').join(params.stand || ''),
+    linkedModulePath = ({ path, directory}) => `${path}/linked_modules/${directory}`;
 
 const envParams = modules.filter(
     ({ param }) => !!param,
 ).reduce(
-    (result, { name, param }) => (result[name] = param, result),
+    (result, { directory, param }) => (result[directory] = param, result),
     {},
 );
 
-actions['initialize'] = params => modules.map(({ name, ...module }) => ({
-    path: modulePath(name),
-    ...module, 
-})).reduce(
+actions['initialize'] = params => modules.reduce(
     (result, {
-        module,
-        path,
+        repository,
+        directory,
         branch = 'stand-{stand}',
-    }) => result.concat(
-        [`git config --global --add safe.directory ${path}`].
-            concat(!fs.existsSync(path) ? [
-                () => mkdir(path),
-                `cd ${path} && git clone${
-                    params.stand ? ` --branch ${replaceWithStand({ value: branch, params })}` : ''
-                } git@gitlab.uis.dev:${module}.git .`
-            ] : []).
-            concat(!fs.existsSync(`${path}/node_modules`) ? [
-                `cd ${path} && npm set registry http://npm.dev.uis.st:80`,
-                `cd ${path} && npm install --verbose`
-            ].
-            concat(actions['fix-permissions']) : []),
-    ),
+        linkedModules = [],
+    }) => {
+        const path = modulePath(directory);
+
+        const clone = ({ path, repository }) => !fs.existsSync(path) ? [
+            () => mkdir(path),
+
+            `cd ${path} && git clone${
+                params.stand ? ` --branch ${replaceWithStand({ value: branch, params })}` : ''
+            } git@gitlab.uis.dev:${repository}.git .`
+        ] : [];
+
+        const cloneLinkedModules = ({ path, linkedModules }) => linkedModules?.reduce((result, {
+            repository,
+            directory,
+            linkedModules,
+        }) => {
+            path = linkedModulePath({ path, directory });
+
+            return result.concat(clone({
+                path,
+                repository,
+            })).concat(cloneLinkedModules({ linkedModules, path }));
+        }, [])
+
+        return result.concat(
+            [`git config --global --add safe.directory ${path}`].
+                concat(clone({ path, repository })).
+                concat(!fs.existsSync(`${path}/node_modules`) ? [
+                    `cd ${path} && npm set registry http://npm.dev.uis.st:80`,
+                ].concat(cloneLinkedModules({ path, linkedModules })).concat((linkedModules?.length ? [() => {
+                    const packageJsons = [];
+
+                    const modifyPackageJson = ({ path, linkedModules }) => {
+                        if (!linkedModules) {
+                            return;
+                        }
+                        
+                        const packageJsonPath = `${path}/package.json`,
+                            backupPath = `${path}/package-backup.json`,
+                            packageJson = readJson(packageJsonPath);
+
+                        !fs.existsSync(backupPath) && write(backupPath, packageJson);
+
+                        packageJsons.push({
+                            path,
+                            packageJson,
+                        });
+
+                        linkedModules.forEach(({
+                            module,
+                            directory,
+                            linkedModules,
+                        }) => {
+                            packageJsons.forEach(
+                                ({ packageJson, path: basePath }) => {
+                                    [
+                                        'dependencies',
+                                        'peerDependencies',
+                                    ].forEach(key => module in (packageJson[key] || {}) && (
+                                        packageJson[key][module] = linkedModulePath({
+                                            path: `.${path.substr(basePath.length)}`,
+                                            directory,
+                                        })
+                                    ));
+                                } 
+                            );
+
+                            modifyPackageJson({
+                                path: linkedModulePath({ path, directory }),
+                                linkedModules,
+                            });
+                        });
+
+                        write(packageJsonPath, encodeJson(packageJson));
+                    };
+
+                    modifyPackageJson({ path, linkedModules });
+                }] : [])/*.concat([
+                    `cd ${path} && npm install --verbose`
+                ])*/.
+                concat(actions['fix-permissions'])) : []),
+        );
+    },
     [],
 );
 
@@ -88,13 +170,13 @@ actions['set-env'] = params => [
             values = require(envJson);
 
         modules.forEach(({
-            module,
-            name,
+            repository,
+            directory,
             envFileName = '.env.local',
-        }) => write(`${modulePath(name)}/${envFileName}`, Object.entries({
+        }) => write(`${modulePath(directory)}/${envFileName}`, Object.entries({
             ...(values.common || {}),
             ...(values[key]?.common || {}),
-            ...(values[key]?.[name] || {}),
+            ...(values[key]?.[directory] || {}),
             ...(local(params)).reduce(
                 (result, name) => {
                     const envParam = envParams[name];
@@ -110,18 +192,23 @@ actions['set-env'] = params => [
     }
 ];
 
-actions['remove-node-modules'] = modules.map(({ module, name }) => rmVerbose(`${modulePath(name)}/node_modules`));
-actions['clear'] = modules.map(({ module, name }) => rmVerbose(modulePath(name)));
+actions['remove-node-modules'] = params => (
+    params.module ?
+        modules.filter(({ directory }) => params.module.includes(directory)) :
+        modules
+).map(({ repository, directory }) => rmVerbose(`${modulePath(directory)}/node_modules`));
+
+actions['clear'] = modules.map(({ repository, directory }) => rmVerbose(modulePath(directory)));
 actions['bash'] = [];
 
 actions['run'] = params =>
     actions['initialize'](params).
     concat(actions['set-env'](params)).
-    concat(modules.reduce((result, { name, script = 'start' }) => {
-        const path = modulePath(name),
+    concat(modules.reduce((result, { directory, script = 'start' }) => {
+        const path = modulePath(directory),
             serverLog = `${path}/server.log`;
 
-        if (name != 'desktop' && !local(params).includes(name)) {
+        if (directory != 'desktop' && !local(params).includes(directory)) {
             return result;
         }
 
@@ -142,6 +229,9 @@ const {action, ...params} = (new Args({
     local: {
         validate: isListOf.apply(null, submoduleNames),
     },
+    module: {
+        validate: isListOf.apply(null, moduleNames),
+    }
 })).createObjectFromArgsArray(process.argv);
 
 if (!action) {
