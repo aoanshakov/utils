e8a6797b98c5d21e11b332d85df19284320fad36
Alexander Anshakov <a.anshakov@uiscom.ru>
2023-12-20 21:01:46 +0000

    Исправь патчи

--- softphone-plugin/tests.tsx
+++ softphone-plugin/tests.tsx
@@ -3,39 +3,8 @@ import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { notification } from 'magic-ui';
 
-import { eventBus, isElectron } from '@comagic/core';
-import Root from '@/bootstrap';
-
-import { createRootStore as createSoftphoneRootStore } from 'softphone/src/models/RootStore';
-import { createRootStore as createChatsRootStore } from 'chats/src/models/RootStore';
-import { createRootStore as createContactsRootStore } from 'contacts/src/models/RootStore';
-
-import { intl } from './i18n';
-import { createMemoryHistory } from 'history';
-
-import { App as Softphone } from '@/applications/softphone';
-import { App as ComagicApp } from '@/applications/default';
-
-import { createRootStore } from '@models/default/RootStore';
-import { createRootStore as createElectronRootStore } from '@models/softphone/RootStore';
-
-const TestBody = ({children}) => <div className="cm-test-body">{children}</div>,
-    Window = ({children}) => <div className="cm-test-window">{children}</div>,
-    history = createMemoryHistory();
+import App from '@/App';
 let root;
-
-const Fiber = () => {
-    const [counter, setCounter] = useState(0);
-
-    return <div>
-        <p>Clicks count: {counter}</p>
-        <button onClick={useCallback(() => setCounter(counter + 1), [counter])}>Increment</button>
-    </div>;
-};
-
-const units = {
-    fiber: Fiber,
-};
 
 window.application = {
     run({
@@ -49,16 +18,8 @@ window.application = {
         const Unit = units[appName];
         let rootStore;
 
-        if (!Unit) {
-            setReactDOM(ReactDOM);
-            setEventBus(eventBus);
-            rootStore = (isElectron() ? createElectronRootStore : createRootStore)();
-            setChatsRootStore(createChatsRootStore());
-            createSoftphoneRootStore();
-            createContactsRootStore();
-            setHistory(history);
-            setNotification(notification);
-        }
+        setReactDOM(ReactDOM);
+        setNotification(notification);
          
         this.exit();
 
@@ -67,10 +28,7 @@ window.application = {
         document.body.appendChild(container);
 
         root = createRoot(document.getElementById('root')),
-
-        root.render(Unit ? <TestBody><Unit /></TestBody> : <Root {...{ rootStore, history }}>
-            {isElectron() ? <Softphone /> : <ComagicApp />}
-        </Root>);
+        root.render(<App />);
     },
 
     exit() {
